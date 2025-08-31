0952033a3b27ee4847d947b43de80bc02a557824
mayahNeko <mayahibela@gmail.com>
2025-08-31 18:39:09 +0300

    feat: add new functionality

--- src/main/java/todolist/service/TodoService.java
+++ src/main/java/todolist/service/TodoService.java
@@ -1,9 +1,38 @@
 package todolist.service;
 
 import todolist.entity.Todo;
+import
 
 import java.time.Instant;
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import todolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+
+    private final TodoRepository repository;
+
+    public TodoService(TodoRepository repository) {
+        this.repository = repository;
+    }
+    public List<Todo> getAll() {
+        return repository.findAll();
+    }
+
+    public Todo postTodo(Todo todo) {
+        return repository.save(todo);
+    }
+
+    public void deleteTodo(int id) {
+        Todo todo = repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo not found"));
+        repository.delete(todo);
+    }
+}
 
 public class TodoService {
     public List<Todo> getAll(){
