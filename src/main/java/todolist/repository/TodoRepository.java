762337a1f43e0f7cd35c428e99cfe781aeb66884
mayahNeko <mayahibela@gmail.com>
2025-08-31 21:56:43 +0300

    delete: jpa field

--- src/main/java/todolist/repository/TodoRepository.java
+++ src/main/java/todolist/repository/TodoRepository.java
@@ -1,9 +1,19 @@
 package todolist.repository;
 
 import todolist.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
-public interface TodoRepository extends JpaRepository<Todo, Integer> {
+public class TodoRepository {
+    private final String url = "jdbc:postgresql://localhost/5432/todo";
+    private final String user = "user";
+    private final String password = "password";
+
+    public Connection connect() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
